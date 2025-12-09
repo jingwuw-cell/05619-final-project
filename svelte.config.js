26b158d4d00e3d69590df9e75f23f1ade424a249
VCplus-512 <jingwuw@andrew.cmu.edu>
2025-12-09 18:56:50 -0500

    use static adapter with fallback for GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -2,16 +2,16 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-
 const dev = process.argv.includes('dev');
-// 仓库名：如果你是 05619-final-project，就写下面这个
 const base = dev ? '' : '/05619-final-project';
 
 const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      fallback: 'index.html'
+    }),
     paths: {
       base
     },
